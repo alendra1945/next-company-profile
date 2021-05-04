53afa5c554eecf9912185b964ddaff8c0977f4f8
alendra1945 <dwialendraipangselly23@gmail.com>
2021-05-04 15:57:54 +0800

    add hooks

---
+++ src/hooks/useVisible.ts
@@ -0,0 +1,23 @@
+import { useState, useEffect, useRef } from 'react';
+
+function useVisible() {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  const handleClickOutside = (event) => {
+    if (ref.current && !ref.current.contains(event.target)) {
+      setIsVisible(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleClickOutside, true);
+    return () => {
+      document.removeEventListener('click', handleClickOutside, true);
+    };
+  }, []);
+
+  return { ref, isVisible, setIsVisible };
+}
+
+export default useVisible;
